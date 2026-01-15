a70bd02d0d677833c28f4988da1c370fdfd54489
MarioA96 <marioaquirozs@gmail.com>
2026-01-14 22:53:26 -0600

    Refactor main function and improve file handling in todo list app. Added functionality for deleting content based on the id of the file.

--- todo-list-app/src/main.rs
+++ todo-list-app/src/main.rs
@@ -1,14 +1,21 @@
-use std::{fs::{self, File, OpenOptions}, io::{self, BufRead, BufReader}, path::Path};
-use std::io::Write;
 use colored::Colorize;
-
+use std::io::Write;
+use std::{
+    fs::{self, File, OpenOptions},
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
 
 const FILE_NAME: &str = "TODOS.txt";
 const PATH: &str = "./";
 
 fn main() {
-    
-    println!("{}", "Todo List App\nChecking for existing file...\n".bold().purple());
+    println!(
+        "{}",
+        "\n\nTodo List App\nChecking for existing file...\n"
+            .bold()
+            .purple()
+    );
 
     if !on_check_file() {
         println!("No {FILE_NAME} found!");
@@ -17,11 +24,12 @@ fn main() {
 
         println!("A new file: {FILE_NAME} has been made\n");
     }
-    
-    loop {
 
+    loop {
         println!("{}", "Seleccione una opcion:".bold().yellow());
-        println!("1.- Mostrar lista de TODOs, 2.- Agregar nuevo TODO, 3.- Eliminar un TODO, 4.- Salir");
+        println!(
+            "1.- Mostrar lista de TODOs\n2.- Agregar nuevo TODO\n3.- Eliminar un TODO\n4.- Salir"
+        );
 
         let mut opcion = String::new();
 
@@ -33,44 +41,46 @@ fn main() {
             "1" => {
                 println!("{}", "\nDisplaying list of TODOs:".bold().blue());
                 print_todos();
-            },
+            }
             "2" => {
-                if on_add_new_todo(){
+                if on_add_new_todo() {
                     println!("New TODO added succesfuly!\n");
                 } else {
                     println!("Adding new TODO failed!\n");
                 }
-            },
+            }
             "3" => {
                 let mut id_todo = String::new();
 
                 println!("\nEnter the number of TODO to DELETE: ");
 
-                io::stdin().read_line(&mut id_todo).expect("Failed to read line");
-                let id_todo: i32 = id_todo.trim().parse().expect("Failed to read entry <Not a number. Entry must be an integer>");
+                io::stdin()
+                    .read_line(&mut id_todo)
+                    .expect("Failed to read line");
+                let id_todo: i32 = id_todo
+                    .trim()
+                    .parse()
+                    .expect("Failed to read entry <Not a number. Entry must be an integer>");
 
-                println!("{}","\nRemoving a TODO from the list...".bold().blue());
+                println!("{}", "\nRemoving a TODO from the list...".bold().blue());
 
-                if on_delete_todo(id_todo){
+                if on_delete_todo(id_todo) {
                     println!("{}", "Deleted TODO succesfuly!\n".green());
                 } else {
                     println!("Deleting TODO failed!\n");
                 }
-            },
+            }
             "4" => {
                 println!("Exiting the app...");
                 return;
-            },
+            }
             _ => println!("Invalid operation!"),
         }
-
     }
-
 }
 
-
 fn on_check_file() -> bool {
-    let path = PATH.to_string()+FILE_NAME;
+    let path = PATH.to_string() + FILE_NAME;
     let path = path.as_str();
 
     if !Path::new(path).exists() {
@@ -81,38 +91,42 @@ fn on_check_file() -> bool {
 }
 
 fn on_create_new_file() {
-    let path = PATH.to_string()+FILE_NAME;
+    let path = PATH.to_string() + FILE_NAME;
     let path = path.as_str();
 
     fs::write(path, "").expect("Failed to write to file");
 }
 
 fn print_todos() {
+    let path = PATH.to_string() + FILE_NAME;
+    let path = path.as_str();
 
-    let data = fs::read(PATH.to_string()+FILE_NAME).expect("Should be able to read file");
+    let reader = BufReader::new(
+        File::open(path)
+            .expect("Couldn't READ on the file <file stream failed, moved or deleted file?>"),
+    );
 
-    if data.len() == 0 {
-        println!("{}","No TODOS to display <List is empty>\n".bold().red());
-    } else {
-        match String::from_utf8(data) {
-            Ok(text) => println!("{}", text.italic().green()),
-            Err(_) => println!("{}", "Error: Could not decode file contents as UTF-8\n".bold().red()),
-        }
+    for (index, line) in reader.lines().enumerate() {
+        let i = index + (1 as usize);
+        println!("{i}.- {}", line.expect("Failed while retrieving line"));
     }
 
+    println!("\n");
 }
 
 fn get_number_lines_file() -> i32 {
-    if !on_check_file(){
+    if !on_check_file() {
         eprintln!("Couldn't write on the file <file stream failed, moved or deleted file?>");
 
         return -1;
     } else {
-        let path = PATH.to_string()+FILE_NAME;
+        let path = PATH.to_string() + FILE_NAME;
         let path = path.as_str();
 
-        let file = BufReader::new(File::open(path)
-                    .expect("Couldn't READ on the file <file stream failed, moved or deleted file?>"));
+        let file = BufReader::new(
+            File::open(path)
+                .expect("Couldn't READ on the file <file stream failed, moved or deleted file?>"),
+        );
 
         // println!("Number of lines: {}", file.lines().count());
 
@@ -120,17 +134,16 @@ fn get_number_lines_file() -> i32 {
     }
 }
 
-fn on_add_new_todo() -> bool{
-
+fn on_add_new_todo() -> bool {
     println!("\nSet the title of the new TODO");
 
     let mut title = String::new();
 
-    io::stdin().read_line(&mut title)
+    io::stdin()
+        .read_line(&mut title)
         .expect("Error while reading line!");
 
-
-    let path = PATH.to_string()+FILE_NAME;
+    let path = PATH.to_string() + FILE_NAME;
     let path = path.as_str();
 
     println!("\nAdding a new TODO to the list...");
@@ -140,8 +153,9 @@ fn on_add_new_todo() -> bool{
 
         return false;
     } else {
-        let num_lines = (get_number_lines_file() + 1).to_string();
-        let new_title = num_lines+".- "+title.as_str();
+        // let num_lines = (get_number_lines_file() + 1).to_string();
+        // let new_title = num_lines + ".- " + title.as_str();
+        let new_title = title.as_str();
 
         let mut file = OpenOptions::new()
             .append(true)
@@ -149,14 +163,50 @@ fn on_add_new_todo() -> bool{
             .expect("Error while trying to do operation over file <permission denied, not enough privileges?>");
 
         write!(file, "{}", new_title).expect("Failed to append to the file");
-        
+
         return true;
     }
 }
 
-fn on_delete_todo(id: i32) -> bool{
+fn on_delete_todo(id: i32) -> bool {
+    if !on_check_file() {
+        eprintln!("Couldn't take action on the file <file stream failed, moved or deleted file?>");
 
-    println!("ID entered: {}", id);
+        return false;
+    } else {
+        if id > get_number_lines_file() || id < 0 {
+            eprintln!("The id entered doesnt exist in the list <is the id correct?>");
 
-    return true;
+            return false;
+        } else {
+            let path = PATH.to_string() + FILE_NAME;
+            let path = path.as_str();
+
+            let reader =
+                BufReader::new(File::open(path).expect(
+                    "Couldn't READ on the file <file stream failed, moved or deleted file?>",
+                ));
+
+            let mut lines: Vec<String> = Vec::new();
+
+            for line in reader.lines() {
+                let line = line.expect("Couldnt retrieve line");
+
+                lines.push(line);
+            }
+
+            let filtered_lines: Vec<String> = lines
+                .into_iter()
+                .enumerate()
+                .filter(|&(index, _)| ((id - 1) as usize) != index)
+                .map(|(_, element)| element)
+                .collect();
+
+            let filtered_lines = filtered_lines.join("\n");
+
+            fs::write(path, filtered_lines + "\n").expect("Failed to write to file");
+
+            return true;
+        }
+    }
 }
