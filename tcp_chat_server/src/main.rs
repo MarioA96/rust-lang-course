b2e86a22ef8b59f76f976fea52489464ea79dd0a
MarioA96 <marioaquirozs@gmail.com>
2026-01-29 22:15:15 -0600

    Refactor connection handling in TCP chat server for improved readability

--- tcp_chat_server/src/main.rs
+++ tcp_chat_server/src/main.rs
@@ -1,15 +1,22 @@
 // TCP Chat Application: Build a multi-client chat server using TCP sockets. Clients connect and broadcast messages. 
 // Learn: Networking with std::net, threads for concurrency, and mutexes for shared state.
 
-use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::{
+    io::prelude::*, 
+    net::{SocketAddr, TcpListener, TcpStream}
+};
 
 fn handle_connection(stream: TcpStream){
+    // let mut buf = [0; 10];
+    // let len = stream.peek(&mut buf).expect("peek failed");
+    // println!("len: {}", len);
+    let mut s = &stream;
+
     println!("Connected to the server!");
     
-    let mut buf = [0; 10];
-    let len = stream.peek(&mut buf).expect("peek failed");
-
-    println!("len: {}", len);
+    let mut buffer = [0; 128];
+    let printable = s.read(&mut buffer).expect("Couldnt resolve"); // Read response
+    println!("printable: {}", printable);
 }
 
 fn main() -> std::io::Result<()> {
