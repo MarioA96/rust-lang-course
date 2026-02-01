ddd1005fd0a2a8f06e7026083056a77020311ba6
MarioA96 <marioaquirozs@gmail.com>
2026-01-31 21:13:03 -0600

    Enhance TCP chat client by adding TTL configuration and improving error handling in non-blocking mode

--- tcp_chat_client/src/main.rs
+++ tcp_chat_client/src/main.rs
@@ -12,30 +12,54 @@ fn main() -> std::io::Result<()> {
     ];
 
     let mut stream = TcpStream::connect(&addrs[..])?;
+
+    // Set the TTL value
+    stream.set_ttl(64).expect("Failed to set TTL");
+
+    // Optionally, you can check the TTL value
+    let ttl = stream.ttl().expect("Failed to get TTL");
+    println!("Current TTL: {}", ttl);
+
+    // We set the current connection to nonblocking status
     stream.set_nonblocking(true).expect("set_nonblocking call failed");
 
     let mut buf: Vec<u8> = vec![];
     loop {
         match stream.read_to_end(&mut buf) {
-            Ok(_) => break,
+            // Server is disconnected
+            Ok(0) => {
+                break;
+            }
+            Ok(_) => {
+                read_data(buf);
+                break;
+            },
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                 // wait until network socket is ready, typically implemented
                 // via platform-specific APIs such as epoll or IOCP\\
+                eprintln!("An exception has occured while attempting to resolve nonblocking status");
+                break;
             }
             Err(e) => panic!("encountered IO error: {e}"),
         };
     };
+    
+    Ok(())
+}
 
+fn read_data(buf: Vec<u8>){
     println!("bytes: {buf:?}");
 
     let ascii_table_upper = vec![
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z',
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
     ];
     let ascii_table_lower = vec![
-        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
     ];
     for b in buf {
-        if b >= 65 && b <= 90{
+        if b == 32{
+            print!(" ")
+        }else if b >= 65 && b <= 90{
             let val = b - 65 as u8;
             print!("{}", ascii_table_upper[ val as usize ]);
         } else if b >= 97 && b <= 122 {
@@ -44,22 +68,4 @@ fn main() -> std::io::Result<()> {
         }
     }
     println!("");
-
-    // let mut buffer = String::new();
-    // let printable = stream.read_to_string(&mut buffer)?; // Read response
-
-    // println!("printable: {}", printable);
-
-
-    // println!("Introduce the message: ");
-
-    // let mut message = String::new();
-    // io::stdin()
-    //     .read_line(&mut message)
-    //     .expect("Couldnt read line");
-
-    // stream.write(message.as_bytes())?; // Send data
-    
-
-    Ok(())
 }
