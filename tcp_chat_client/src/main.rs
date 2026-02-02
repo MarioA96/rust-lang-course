492f873269eecb85f6a763cafed9b71376321caa
MarioA96 <marioaquirozs@gmail.com>
2026-02-02 03:34:23 -0600

    Refactor TCP chat client to improve stream handling and simplify data reading

--- tcp_chat_client/src/main.rs
+++ tcp_chat_client/src/main.rs
@@ -13,7 +13,8 @@ fn main() -> std::io::Result<()> {
         SocketAddr::from(([127,0,0,1], 4343)),
     ];
 
-    let stream = TcpStream::connect(&addrs[..])?;
+    let mut stream = TcpStream::connect(&addrs[..])?;
+    let mut reader = BufReader::new(stream.try_clone()?);
 
     // Set the TTL value
     stream.set_ttl(64).expect("Failed to set TTL");
@@ -23,9 +24,7 @@ fn main() -> std::io::Result<()> {
     println!("Current TTL: {}", ttl);
 
     // We set the current connection to nonblocking status
-    stream.set_nonblocking(true).expect("set_nonblocking call failed");
-
-    let mut reader = BufReader::new(stream.try_clone()?);
+    // stream.set_nonblocking(true).expect("set_nonblocking call failed");
 
     thread::spawn(move || {
         let mut buf = String::new();
@@ -35,33 +34,32 @@ fn main() -> std::io::Result<()> {
                 // Server is disconnected
                 Ok(0) => {
                     break;
-                }
-                Ok(_) => {
-                    read_data(buf);
-                    break;
                 },
+
+                Ok(_) => println!("bytes: {buf:?}"),
+
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                     // wait until network socket is ready, typically implemented
                     // via platform-specific APIs such as epoll or IOCP\\
                     eprintln!("An exception has occured while attempting to resolve nonblocking status");
                     break;
-                }
+                },
+
                 Err(e) => panic!("encountered IO error: {e}"),
             };
         };
     });
 
     // Main thread para enviar mensajes
-    // let stdin = io::stdin();
+    let stdin = io::stdin();
 
-    // for line in stdin.lock().lines() {
-    //     let msg = line?;
-    //     writeln!(stream, "{}", msg)?;
-    // }
+    let addr = stream.local_addr().expect("Couldnt resolve local addr");
+
+    for line in stdin.lock().lines() {
+        let msg = line?;
+        // print!("Client: {}:{}> ", addr.ip(), addr.port());
+        writeln!(stream, "{}:{} > {}", addr.ip(), addr.port() ,msg)?;
+    }
     
     Ok(())
-}
-
-fn read_data(buf: String) {
-    println!("bytes: {buf:?}");
 }
