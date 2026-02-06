b4f7bf6d72d235451cbab7dfcb777dab4d1e047b
MarioA96 <marioaquirozs@gmail.com>
2026-02-05 21:28:08 -0600

    Refactor handle_connection to manage client connections and broadcast messages

--- tcp_chat_server/src/main.rs
+++ tcp_chat_server/src/main.rs
@@ -1,56 +1,73 @@
 // TCP Chat Application: Build a multi-client chat server using TCP sockets. Clients connect and broadcast messages. 
 // Learn: Networking with std::net, threads for concurrency, and mutexes for shared state.
 
+use std::collections::HashSet;
 use std::io;
 use std::io::BufReader;
+use std::sync::{Arc, Mutex};
 use std::{
     io::prelude::*, 
     net::{SocketAddr, TcpListener, TcpStream},
 };
 use std::thread;
 
-fn handle_connection(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream, clients: Arc<Mutex<HashSet<SocketAddr>>>) {
     // We set the current connection to nonblocking status
     // we want to handle multiple clients without blocking the server
     // and also to listen to messages from clients asynchronously and sending responses
     stream.set_nonblocking(true).expect("set_nonblocking call failed");
 
     // We receive the local address of the incoming connection
-    let addr = stream.peer_addr().expect("Couldnt resolve for incoming address <ScoketAddr>");
-    println!("Incoming connection from: {}:{}", addr.ip(), addr.port());
+    let client_addr = stream.peer_addr().expect("Couldnt resolve for incoming address <ScoketAddr>");
+    println!("Incoming connection from: {}:{}", client_addr.ip(), client_addr.port());
+
+    // We add the new client to the shared state
+    clients.lock().unwrap().insert(client_addr);
+    println!("Current connected clients: {:?}", clients.lock().unwrap());
 
     // We send a response to the client to confirm the connection
-    let welcome_message = format!("Connected! Your address is {}:{}\n", addr.ip(), addr.port());
+    let welcome_message = format!("Connected! Your address is {}:{}\n", client_addr.ip(), client_addr.port());
     stream.write(welcome_message.as_bytes()).expect("Couldnt send welcome message");
 
-    // Then we create a BufReader to read new incoming messages from the client
+
     let mut reader = BufReader::new(stream.try_clone().expect("Error while attempting to read stream"));
-    // We spawn a new thread to handle incoming messages from the client in order to not block the main server thread
-    thread::spawn(move || {
-        let mut buf = String::new();
-        loop {
-            buf.clear();
-            match reader.read_line(&mut buf) {
-                // Client is disconnected
-                Ok(0) => {
-                    println!("Client {}:{} disconnected", addr.ip(), addr.port());
-                    break;
-                },
+    let mut buf = String::new();
+    loop {
+        buf.clear();
+        match reader.read_line(&mut buf) {
+            // Client is disconnected
+            Ok(0) => {
+                println!("Client {}:{} disconnected", client_addr.ip(), client_addr.port());
 
-                Ok(_) => print!("Message from {}:{}> {}", addr.ip(), addr.port(), buf),
+                clients.lock().unwrap().remove(&client_addr);
+                println!("Current connected clients: {:?}", clients.lock().unwrap());
 
-                // We handle the WouldBlock error to continue the loop
-                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                    // wait until network socket is ready, typically implemented
-                    // via platform-specific APIs such as epoll or IOCP
-                    // Here we just continue the loop
-                    continue;
-                },
+                break;
+            },
+            Ok(_) => {
+                print!("Message from {}:{}> {}", client_addr.ip(), client_addr.port(), buf);
 
-                Err(e) => panic!("encountered IO error: {e}"),
-            };
+                // Broadcast the message to all connected clients
+                let clients_guard = clients.lock().unwrap();
+
+                // Iterate over all clients and send the message
+                for &addr in clients_guard.iter() {
+                    if addr != client_addr {
+                        if let Ok(mut client_stream) = TcpStream::connect(addr) {
+                            let message_with_newline = format!("Message from {}:{}> {}", client_addr.ip(), client_addr.port(), buf);
+                            client_stream.write(message_with_newline.as_bytes()).expect("Couldnt send message to client");
+                        }
+                    }
+                }
+            },
+            // We handle the WouldBlock error to continue the loop
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            },
+            Err(e) => panic!("encountered IO error: {e}"),
         };
-    });
+    }
+
 }
 
 fn main() -> std::io::Result<()> {
@@ -62,6 +79,9 @@ fn main() -> std::io::Result<()> {
     
     //After we bind to the address we create a listener in that socket
     let listener = TcpListener::bind(&addrs[..])?;
+
+    //We create a thread-safe shared state to keep track of connected clients
+    let clients = Arc::new(Mutex::new(HashSet::<SocketAddr>::new()));
 
     //We just print the data of wich socket are we binding the server
     let addr = listener.local_addr().expect("Couldn't bind");
@@ -76,9 +96,10 @@ fn main() -> std::io::Result<()> {
             // If we get a stream we handle the connection in a new thread
             Ok(stream) => {
                 let stream = stream;
+                let clients_clone = Arc::clone(&clients);
 
                 thread::spawn( move || {
-                    handle_connection(stream)
+                    handle_connection(stream, clients_clone)
                 });
             },
             Err(e) => println!("Couldn't get client: {e:?}"),
