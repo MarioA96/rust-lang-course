7939c5e630c15c030511335ae3a60635f184e7ea
MarioA96 <marioaquirozs@gmail.com>
2026-02-19 23:22:39 -0600

    Remove unnecesary comments and added additional iterations in the loop calculation.

--- threads_calc/src/main.rs
+++ threads_calc/src/main.rs
@@ -11,21 +11,6 @@ fn main() -> std::io::Result<()> {
 
     // We spawn 10 threads, each of which will increment the counter 100 times
     calculation(&counter,&mut handles); // We call the calculation function to perform some work before starting the threads
-    // for _ in 0..10 {
-    //     // We clone the Arc to get a new reference to the counter for each thread
-    //     let counter_clone = Arc::clone(&counter);
-    //     // We spawn a new thread and move the cloned Arc into the thread's closure
-    //     let handle = thread::spawn(move || {
-    //         for _ in 0..100 {
-    //             // We lock the mutex to get mutable access to the counter, increment it, and then the lock is automatically released when the scope ends
-    //             let mut num = counter_clone.lock().unwrap();
-    //             // We increment the counter, deferencing the mutex guard to get the actual value of the counter and then incrementing it
-    //             *num += 1;
-    //         }
-    //     });
-    //     // We push the thread handle into the vector so we can join it later
-    //     handles.push(handle);
-    // }
 
     // We join all the threads to ensure they have completed before we print the final counter value
     for handle in handles {
@@ -40,7 +25,7 @@ fn main() -> std::io::Result<()> {
 }
 
 fn calculation(counter: &Arc<Mutex<i32>>, handles: &mut Vec<thread::JoinHandle<()>>) {
-    for _ in 0..10 {
+    for _ in 0..10000 {
         // We clone the Arc to get a new reference to the counter for each thread
         let counter_clone = Arc::clone(counter);
         // We spawn a new thread and move the cloned Arc into the thread's closure
